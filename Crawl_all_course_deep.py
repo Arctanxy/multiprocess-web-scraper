891c75e27ea8a88fc47e7639585edf54e90c8063
Arctanxy <luobowamei@126.com>
2017-07-04 15:26:34 +0800

    Update Crawl_all_course_deep.py

--- Crawl_all_course_deep.py
+++ Crawl_all_course_deep.py
@@ -5,7 +5,8 @@ import time
 
 # Try to crawl all the courses in 163course.
 
-start_url = 'http://www.icourse163.org/'
+start_url_one = 'http://www.icourse163.org/'
+start_url_two = 'http://www.icourse163.org/category/all'
 
 course_url_list = []
 
@@ -14,14 +15,15 @@ headers = {
 }
 #Headers is important
 
-def get_all_course_urls(url)：
+def get_all_course_urls(url，i)：
+  if i >= 4:
+    return None
   time.sleep(2)
-  r = requests.get(start_url,headers = headers)
+  r = requests.get(url,headers = headers)
 
   tree = lxml.html.fromstring(r.content)
 
   urllist = tree.xpath('//a/@href')
-
 
   for item in urllist:
     if 'http://www.icourse163.org/course/' in item and item not in course_url_list:
@@ -29,6 +31,14 @@ def get_all_course_urls(url)：
     else:
       #If the url doesn't meet the form of course url, we should crawl inside to the url
       #It will be easier to do this by Scrapy, but we will make it by ourselves
-      get_all_course_urls(item)
- 
+      get_all_course_urls(item,i+1)
+
+      
+      
+      
+if __name__ == '__main__':
+  p1 = multiprocessing.Process(target = get_all_course_urls,args=(start_url_one,1))
+  p2 = multiprocessing.Process(target = get_all_course_urls,args=(start_url_two,1))
+  p1.start()
+  p2.start()
 
